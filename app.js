7092789c8402c4baf85b5489fa665022adc41176
Jane-Kotovich <jenyakotovich@gmail.com>
2022-08-27 16:56:43 +1000

    Setting up basic routes

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'hello from the server!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint ...');
+});
+
+app.listen(PORT, () => {
+  console.log('App running on port 3000');
+});
