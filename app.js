f29f2da5bdb97250c8f26bc89a8739053d5183eb
Jane-Kotovich <jenyakotovich@gmail.com>
2022-09-04 18:17:11 +1000

    Handled PATCH req

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ app.get('/api/v1/tours/:id', (req, res) => {
   res.status(200).json({ status: 'success', data: { tour } });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  res
+    .status(200)
+    .json({ status: 'success', data: { tour: '<Updated tour here ...>' } });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
