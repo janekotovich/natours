ccd7904519af6e7c71785faa4bcd1314a8fe2c6a
Jane-Kotovich <jenyakotovich@gmail.com>
2022-09-12 18:55:13 +1000

    Adding param middleweare

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -38,16 +45,10 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
   res
     .status(200)
     .json({ status: 'success', data: { tour: '<Updated tour here ...>' } });
 };
 exports.deleteTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
-  }
   res.status(204).json({ status: 'success', data: null });
 };
